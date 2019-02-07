51451360c52a8e8bb9feccb907e69bc105dd939f
popcor255 <popcor255@gmail.com>
2019-02-07 13:33:05 -0500

    remove scroll, unstable. made fixes

--- src/js/events.js
+++ src/js/events.js
@@ -7,36 +7,27 @@ nav = document.getElementById("mySidenav");
 nav.style.left = "0px";
 containers = document.getElementsByClassName("chart_wrapper");
 
+Hammer(nav_wrapper).on("swipeleft", function() {
+  slideIn();
+});
 
-Hammer(nav_wrapper).on('swipeleft', function(){
+Hammer(nav_wrapper).on("swiperight", function() {
+  slideOut();
+});
+
+window.onkeyup = function(e) {
+  var key = e.keyCode ? e.keyCode : e.which;
+  if (key == 37) {
     slideIn();
- });
- 
-Hammer(nav_wrapper).on('swiperight', function(){
+  } else if (key == 39) {
     slideOut();
- });
+  }
+};
 
+function slideIn() {
+  nav.style.left = "-120px";
+}
 
- window.onkeyup = function(e) {
-    var key = e.keyCode ? e.keyCode : e.which;
-    if(key == 37){
-        slideIn();
-    }
-    else if(key == 39){
-        slideOut();
-    }
- }
-
- function slideIn(){
-    for(var x = 0; x < containers.length; x++){
-        containers[x].style.paddingLeft = "0px";
-    }
-    nav.style.left = "-120px";
- }
-
- function slideOut(){
-    for(var x = 0; x < containers.length; x++){
-        containers[x].style.paddingLeft = "125px";
-    }
-    nav.style.left = "0px";
- }
+function slideOut() {
+  nav.style.left = "0px";
+}
--- src/js/main.js
+++ src/js/main.js
@@ -9,215 +9,225 @@ var api;
 var stocks;
 var data;
 var results = null;
-var intervals = 'daily';
+var intervals = "daily";
 var amount = 35;
 var current_price = 0;
 var current_stock = null;
 var options = null;
-var stock_list = ['GOOGL', 'TSLA', 'FB', 'JNJ', 'SPY', 'AMZN', 'AAPL','GPS'];
+var stock_list = ["GOOGL", "TSLA", "FB", "JNJ", "SPY"];
+var loaded = new Set();
 var stock_index = 0;
 
-function loadScript(){
-  //api token 
+function loadScript() {
+
+}
+
+function loadStock(index){
+    //api token
+  if(!loaded.has(index)){
+    loaded.add(index);
     stocks = new Stocks("XNVNORVDGFRLHC00");
     container = document.getElementById("container");
-    getResults();
-};
+    getResults(index);
+  }
 
-function loadCharts(){
-  setTimeout(check, 1000)
 }
 
-async function Results(type, label){
+function loadCharts() {
+  setTimeout(check, 1000);
+}
 
+async function Results(type, label) {
   var ReturnObjs;
 
-  stocks.timeSeries(options).then(
-  function(result) {
+  stocks
+    .timeSeries(options)
+    .then(function(result) {
+      if (result.length == 0) {
+        connection = false;
+      } else {
+        connection = true;
+      }
 
-        if(result.length == 0){
-          connection = false;
-        }
-        else{
-          connection = true;
-        }
-      
-        console.log(result);
-        var v_array = [];
-        var c_array = [];
+      console.log(result);
+      var v_array = [];
+      var c_array = [];
 
-        const monthNames = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-        ];
+      const monthNames = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
 
+      var dateFormat = "MMMM DD YYYY";
+      var dateOffset = 24 * 60 * 60 * 1000 * 2;
+      var d = new Date(result[result.length - 1].date.getTime());
 
-        var dateFormat = 'MMMM DD YYYY';
-        var dateOffset = (24*60*60*1000) * 2;
-        var d = new Date(result[result.length - 1].date.getTime());
+      var date =
+        monthNames[d.getMonth()] + " " + d.getDate() + " " + d.getFullYear();
 
-        var date = monthNames[d.getMonth()] + " " + d.getDate() + " " + d.getFullYear(); 
-      
-        var data = getRandomData(date, amount);
+      var data = getRandomData(date, amount);
 
-        var volume = {  
-          v: number,
-          t: number
-        };
+      var volume = {
+        v: number,
+        t: number
+      };
 
-        current_price = result[result.length - 1].close;
+      current_price = result[result.length - 1].close;
 
-        for(var x = 0; x < result.length; x++){
-          data[x].o = result[x].open;
-          data[x].c = result[x].close;
-          data[x].h = result[x].high;
-          data[x].l = result[x].low;
-          volume.v  = result[x].volume;
-          volume.t  = data[x].t;
-          v_array.push(volume);
-        }
+      for (var x = 0; x < result.length; x++) {
+        data[x].o = result[x].open;
+        data[x].c = result[x].close;
+        data[x].h = result[x].high;
+        data[x].l = result[x].low;
+        volume.v = result[x].volume;
+        volume.t = data[x].t;
+        v_array.push(volume);
+      }
 
-        c_array = data;
+      c_array = data;
 
-        if(type == "candlestick"){
-          setResults(c_array);
-        }
-        else if(type == "volume"){
-          setResults(v_array);
-        }
-        
-        createChart(type, label);
-      
-  }).catch(function(e){
+      if (type == "candlestick") {
+        setResults(c_array);
+      } else if (type == "volume") {
+        setResults(v_array);
+      }
 
-    //error handling logic
-  });
+      createChart(type, label);
+    })
+    .catch(function(e) {
+      //error handling logic
+    });
 }
 
-function getResults(){
-  if(stock_list.length > stock_index){
-    addChart('candlestick', stock_list[stock_index]);
-    if(connection == true){
-      stock_index += 1;
+function getResults(index) {
+  window.setTimeout(function() {
+    if (stock_index < stock_list.length) {
+      addChart("candlestick", stock_list[index]);
     }
-    window.setTimeout(function(){
-      getResults();
-    },3400);
-  }
-  else{
-    stock_index = 0;
-  }
+  }, 200);
   return results;
 }
 
-function setResults(r){
+function setResults(r) {
   results = r;
 }
 
-function addChart(type, label){
-    current_stock = label;
-    setOptions();
-    Results(type, label);
+function addChart(type, label) {
+  current_stock = label;
+  setOptions();
+  Results(type, label);
 }
 
-function createChart(type, label){
-    
-    var obj = null;  
+function createChart(type, label) {
+  var obj = null;
 
-    wrapper = document.createElement("div");
-    wrapper.style.opacity = 0;
-    ele = document.createElement("canvas");
-    wrapper.setAttribute("class", "chart_wrapper");
-    ele.setAttribute("class", "charts");
-    ele.setAttribute("id", "chart_" + chart_index);
-    container.appendChild(wrapper);
-    wrapper.appendChild(ele);
+  wrapper = document.createElement("div");
+  wrapper.style.opacity = 0;
+  ele = document.createElement("canvas");
+  wrapper.setAttribute("class", "chart_wrapper");
+  ele.setAttribute("class", "charts");
+  ele.setAttribute("id", "chart_" + chart_index);
+  container.appendChild(wrapper);
+  wrapper.appendChild(ele);
 
-    if(type == "candlestick"){
-      obj = candlestick();  
-    }
+  if (type == "candlestick") {
+    obj = candlestick();
+  }
 
-    charts[chart_index] = new Chart(document.getElementById("chart_" + chart_index), obj);      
+  charts[chart_index] = new Chart(
+    document.getElementById("chart_" + chart_index),
+    obj
+  );
 
+  window.setTimeout(function() {
+    wrapper.style.opacity = 1;
+  }, 200);
 
-    window.setTimeout(function(){
-      wrapper.style.opacity = 1;
-    },200);
+  chart_index++;
 
-    chart_index++;
-
-    return ele;   
+  return true;
 }
 
-function candlestick(){
-    var obj = {
-      type: 'candlestick',
-      data: {
-        datasets: [{ 
+function candlestick() {
+  var obj = {
+    type: "candlestick",
+    data: {
+      datasets: [
+        {
           color: {
-            down: '#80ff80',
-            up: '#ff6666',
-            unchanged: '#999',
+            down: "#80ff80",
+            up: "#ff6666",
+            unchanged: "#999"
           },
-          borderColor: '#000',
+          borderColor: "#000",
           borderWidth: 2,
-            label: "(" + current_stock + ") " + current_price,
-            data: results
-          }]
-      },
-      options: {
-				tooltips: {
-					position: 'nearest',
-					mode: 'index',
-				},
-			},
-    };
-    return obj;
+          label: "(" + current_stock + ") " + current_price,
+          data: results
+        }
+      ]
+    },
+    options: {
+      tooltips: {
+        position: "nearest",
+        mode: "index"
+      }
+    }
+  };
+  return obj;
 }
 
-function setOptions(){
+function setOptions() {
   options = {
     symbol: current_stock,
     interval: intervals,
     amount: amount
-  }
+  };
 }
 
-function setWholeSet(values){
+function setWholeSet(values) {
   set = values;
 }
 
-function getWholeSet(values){
- return set;
+function getWholeSet(values) {
+  return set;
 }
 
-function setSet(index, value){
+function setSet(index, value) {
   set[index] = value;
 }
 
-function getSet(){
- return sets[0];
+function getSet() {
+  return sets[0];
 }
 
-function scrollTo(e){
+function scrollTo(e) {
   var ele = document.getElementById(e);
-  if(ele != null){
-    ele.scrollIntoView({ behavior: 'smooth', block: 'center'});
+  if (ele != null) {
+    ele.scrollIntoView({ behavior: "smooth", block: "center" });
   }
 }
 
 function getRandomData(date, count) {
-	var dateFormat = 'MMMM DD YYYY';
-	var date = moment(date, dateFormat);
-	var data = [randomBar(date, 30)];
-	while (data.length < count) {
-		date = date.clone().add(1, 'd');
-		if (date.isoWeekday() <= 5) {
-			data.push(randomBar(date, data[data.length - 1].c));
-		}
-	}
-	return data;
+  var dateFormat = "MMMM DD YYYY";
+  var date = moment(date, dateFormat);
+  var data = [randomBar(date, 30)];
+  while (data.length < count) {
+    date = date.clone().add(1, "d");
+    if (date.isoWeekday() <= 5) {
+      data.push(randomBar(date, data[data.length - 1].c));
+    }
+  }
+  return data;
 }
 
-
 loadScript();
-
